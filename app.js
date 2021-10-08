08f05c2c4c04f6683f1a71ab810e5c6801c0d89a
trieungochai <trieungochai.dev@gmail.com>
2021-10-08 22:32:08 +0700

    Basic Express App

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/", (req, res) => {
+  res.send("<h1>Store API</h1><a href='/api/v1/products'>Products route</a>");
+});
+
+// products route
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ middleware/error-handler.js
@@ -0,0 +1,8 @@
+const errorHandleMiddleware = (err, req, res, next) => {
+  console.log(err);
+  return res
+    .status(500)
+    .json({ msg: "Something went wrong, please try again!" });
+};
+
+module.exports = errorHandleMiddleware;
