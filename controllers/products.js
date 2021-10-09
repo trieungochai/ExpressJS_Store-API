0657731a62774ecbdf5c63427e5c304e0186688b
trieungochai <trieungochai.dev@gmail.com>
2021-10-09 12:49:03 +0700

    Query Params

--- controllers/products.js
+++ controllers/products.js
@@ -5,8 +5,9 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ products });
 };
 
-const getAllProducts = (req, res) => {
-  res.status(200).json({ msg: "Products route" });
+const getAllProducts = async (req, res) => {
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
