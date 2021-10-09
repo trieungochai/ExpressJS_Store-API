1bf78b84babf434ef3ee8a18a61137f5f60809f6
trieungochai <trieungochai.dev@gmail.com>
2021-10-09 13:27:26 +0700

    Pagination

--- controllers/products.js
+++ controllers/products.js
@@ -36,6 +36,12 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldsList);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   // console.log(queryObject);
   const products = await Product.find(req.query);
   res.status(200).json({ products, nbHits: products.length });
